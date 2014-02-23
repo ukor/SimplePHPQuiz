4fa524ed153b0f43495674cc42f5979a58b83bf3
Val Okafor <okaatech@yahoo.com>
2014-02-23 12:26:48 -0800

    Show questions from array

--- index.php
+++ index.php
@@ -1,46 +1,35 @@
 <?php
 include('includes/header.html');
 
-$questions = array(
-        1 => array(
-               'Question'       => 'What does PHP stand for?',
-               'Answer'         => array(   'A' => 'PHP: Hypertext Preprocessor',
-                                            'B' => 'Personal Home Page', 
-                                            'C' => 'Programmers Hosting Platform'),
-                'CorrectAnswer' => 'A'),
-        2 => array(
-               'Question'       => 'What is the population of Los Angeles?',
-               'Answer'         => array(   'A' => '13 Million',
-                                            'B' => '8 Million', 
-                                            'C' => '20 Million'),
-                'CorrectAnswer' => 'B'),
-
-
-            );
-
+$questions = array('What does php stand for?', 'What is the population of Los Angeles?', 'What is the capital of California?');
+$answers = array(
+    array(0 => 'PHP: Hypertext Preprocessor', 'Personal Home Page', 'Programmers Hyper Platform'),
+    array(0 => '8 million', '20 mllion', '9.7 million'),
+    array(0 => 'Sacramento', 'Los Angeles', 'San Francisco')
+    );
 
 ?>
 
 <h1>Val Okafor Simple PHP Quiz</h1>
         <form action="quiz.php" method="post" id="quiz">
-           <?php foreach ($questions as $quizNumber => $value) { ?>
-                <ol>
-                    <li>
-                        <h3><?php echo $value['Question']; ?> </h3>
-                        <?php 
-                            foreach ($value as $letter => $answer) {
-                                $label = 'question-'.$quizNumber.'-answers-'.$letter;
-                            ?>
-                            <div class="form-group">
-                                <input type="radio" name="answers[<?php echo $quizNumber; ?>]" id="<?php echo $label; ?>"
-                                value="<?php echo $letter; ?> " />
-                                
-                                <label for="<?php echo $label; ?>"><?php echo $letter; ?> <?php echo $answer ?></label>
-                            </div>
-                            <?php } ?> 
-                    </li>
-                </ol>
-                <?php } ?>   
+       <?php 
+       $pattern = ' ';
+       $replace = '_';
+       for ($num=0; $num < count($questions) ; $num++) { 
+
+        echo "<div class=\"form-group\">";
+        echo "<label for=\"$questions[$num]\">$questions[$num]</label>" ."<ol>";
+        
+       foreach ($answers[$num] as $answer) {
+           $answer_with_no_space = str_replace($pattern, $replace, $answer);
+           echo "<li><input type=\"radio\" id=\"$answer_with_no_space\" value=\"$answer_with_no_space\" name=\"$answer_with_no_space\" />\n";
+           echo "<label for=\"$answer\">$answer</label></li>\n";}
+       echo "</ol>" . "</div>";          
+       }
+
+
+       ?>   
+      
 
        	<button type="submit" class="btn btn-primary">Submit Quiz</button>
     
